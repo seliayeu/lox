f8cc5b047ff042dc7c21c7f40e04949d9f8003ab
danila seliayeu <dan.seliayeu@gmail.com>
2024-11-26 23:17:33 -0700

    continue Scanning portion of tutorial

--- Scanner.h
+++ Scanner.h
@@ -1,5 +1,66 @@
+#include "Token.h"
 #include <string>
+#include <vector>
+
 class Scanner {
+  const std::string source;
+  std::vector<Token> tokens;
+  int start = 0;
+  int current = 0;
+  int line = 1;
+
+  bool isAtEnd() { return current >= source.length(); }
+  void scanToken() {
+    char c = advance();
+    switch (c) {
+    case '(':
+      addToken(TokenType::LEFT_PAREN);
+      break;
+    case ')':
+      addToken(TokenType::RIGHT_PAREN);
+      break;
+    case '{':
+      addToken(TokenType::LEFT_BRACE);
+      break;
+    case '}':
+      addToken(TokenType::RIGHT_BRACE);
+      break;
+    case ',':
+      addToken(TokenType::COMMA);
+      break;
+    case '.':
+      addToken(TokenType::DOT);
+      break;
+    case '-':
+      addToken(TokenType::MINUS);
+      break;
+    case '+':
+      addToken(TokenType::PLUS);
+      break;
+    case ';':
+      addToken(TokenType::SEMICOLON);
+      break;
+    case '*':
+      addToken(TokenType::STAR);
+      break;
+    }
+  }
+
+  char advance() { return source[current++]; }
+
+  void addToken(TokenType type, std::any literal = nullptr) {
+    std::string text = source.substr(start, current - start);
+    tokens.push_back(Token(type, text, literal, line));
+  }
+
 public:
-  Scanner(std::string) {}
+  Scanner(std::string source) : source(source) {}
+  std::vector<Token> scanTokens() {
+    while (!isAtEnd()) {
+      start = current;
+      scanToken();
+    }
+    tokens.push_back(Token(TokenType::END_OF_LINE, "", nullptr, line));
+    return tokens;
+  }
 };
--- Token.h
+++ Token.h
@@ -2,59 +2,60 @@
 #include <iostream>
 #include <string>
 #include <strstream>
+
+enum TokenType {
+  // Single-character tokens.
+  LEFT_PAREN,
+  RIGHT_PAREN,
+  LEFT_BRACE,
+  RIGHT_BRACE,
+  COMMA,
+  DOT,
+  MINUS,
+  PLUS,
+  SEMICOLON,
+  SLASH,
+  STAR,
+
+  // One or two character tokens.
+  BANG,
+  BANG_EQUAL,
+  EQUAL,
+  EQUAL_EQUAL,
+  GREATER,
+  GREATER_EQUAL,
+  LESS,
+  LESS_EQUAL,
+
+  // Literals.
+  IDENTIFIER,
+  STRING,
+  NUMBER,
+
+  // Keywords.
+  AND,
+  CLASS,
+  ELSE,
+  FALSE,
+  FUN,
+  FOR,
+  IF,
+  NIL,
+  OR,
+  PRINT,
+  RETURN,
+  SUPER,
+  THIS,
+  TRUE,
+  VAR,
+  WHILE,
+
+  END_OF_LINE,
+};
+
 class Token {
 
 public:
-  enum TokenType {
-    // Single-character tokens.
-    LEFT_PAREN,
-    RIGHT_PAREN,
-    LEFT_BRACE,
-    RIGHT_BRACE,
-    COMMA,
-    DOT,
-    MINUS,
-    PLUS,
-    SEMICOLON,
-    SLASH,
-    STAR,
-
-    // One or two character tokens.
-    BANG,
-    BANG_EQUAL,
-    EQUAL,
-    EQUAL_EQUAL,
-    GREATER,
-    GREATER_EQUAL,
-    LESS,
-    LESS_EQUAL,
-
-    // Literals.
-    IDENTIFIER,
-    STRING,
-    NUMBER,
-
-    // Keywords.
-    AND,
-    CLASS,
-    ELSE,
-    FALSE,
-    FUN,
-    FOR,
-    IF,
-    NIL,
-    OR,
-    PRINT,
-    RETURN,
-    SUPER,
-    THIS,
-    TRUE,
-    VAR,
-    WHILE,
-
-    END_OF_LINE,
-  };
-
   const TokenType type;
   const std::string lexeme;
   const std::any literal;
